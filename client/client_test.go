6258fa0f85a2ec576e8762b16d42f68e7daa8af9
Thomas Lu <thomas.lu@mistsys.com>
2018-06-28 10:59:40 -0700

    scaffolding for more client tests

--- client/client_test.go
+++ client/client_test.go
@@ -1,20 +1,51 @@
 package client
 
 import (
+	"context"
 	"net/http"
 	"reflect"
 	"testing"
 )
 
-func TestTenableClient_NewRequest(t *testing.T) {
-	type fields struct {
-		baseURL     string
-		client      *http.Client
-		accessKey   string
-		secretKey   string
-		Debug       bool
-		impersonate string
+func TestResponse_BodyJson(t *testing.T) {
+	tests := []struct {
+		name string
+		r    *Response
+		want string
+	}{
+		// TODO: Add test cases.
 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.r.BodyJson(); got != tt.want {
+				t.Errorf("Response.BodyJson() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewClient(t *testing.T) {
+	type args struct {
+		accessKey string
+		secretKey string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TenableClient
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewClient(tt.args.accessKey, tt.args.secretKey); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewClient() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTenableClient_NewRequest(t *testing.T) {
 	type args struct {
 		method      string
 		relativeUrl string
@@ -22,71 +53,167 @@ func TestTenableClient_NewRequest(t *testing.T) {
 	}
 	tests := []struct {
 		name    string
-		fields  fields
+		t       *TenableClient
 		args    args
 		want    *http.Request
 		wantErr bool
 	}{
-		{ // X-ApiKeys: accessKey={accessKey}; secretKey={secretKey};
-			name: "Requests include the X-ApiKeys headers in required format",
-			fields: fields{
-				baseURL:   "https://cloud.tenable.com",
-				client:    http.DefaultClient,
-				accessKey: "fakeKey",
-				secretKey: "fakeSecret",
-			},
-			args: args{
-				method:      "GET",
-				relativeUrl: "/foo",
-				body:        nil,
-			},
-			want: &http.Request{
-				Header: map[string][]string{
-					// apparently Go downcases the keys
-					"X-Apikeys": []string{"accessKey=fakeKey; secretKey=fakeSecret"},
-				},
-			},
-		},
-		{ // X-ApiKeys: accessKey={accessKey}; secretKey={secretKey};
-			name: "Requests include the X-Impersonate headers in required if Impersonate is set",
-			fields: fields{
-				baseURL:     "https://cloud.tenable.com",
-				client:      http.DefaultClient,
-				accessKey:   "fakeKey",
-				secretKey:   "fakeSecret",
-				impersonate: "fakeUser",
-			},
-			args: args{
-				method:      "GET",
-				relativeUrl: "/foo",
-				body:        nil,
-			},
-			want: &http.Request{
-				Header: map[string][]string{
-					"X-Apikeys":     []string{"accessKey=fakeKey; secretKey=fakeSecret"},
-					"X-Impersonate": []string{"username=fakeUser"},
-				},
-			},
-		},
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tc := &TenableClient{
-				baseURL:     tt.fields.baseURL,
-				client:      tt.fields.client,
-				accessKey:   tt.fields.accessKey,
-				secretKey:   tt.fields.secretKey,
-				Debug:       tt.fields.Debug,
-				impersonate: tt.fields.impersonate,
-			}
-			got, err := tc.NewRequest(tt.args.method, tt.args.relativeUrl, tt.args.body)
+			got, err := tt.t.NewRequest(tt.args.method, tt.args.relativeUrl, tt.args.body)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("TenableClient.NewRequest() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got.Header, tt.want.Header) {
-				t.Errorf("TenableClient.NewRequest() = %v, want %v", got.Header, tt.want.Header)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TenableClient.NewRequest() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestTenableClient_Do(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		req  *http.Request
+		dest interface{}
+	}
+	tests := []struct {
+		name    string
+		t       *TenableClient
+		args    args
+		want    *Response
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.t.Do(tt.args.ctx, tt.args.req, tt.args.dest)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TenableClient.Do() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TenableClient.Do() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTenableClient_Get(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		u    string
+		opts *TenableQueryOpts
+		dest interface{}
+	}
+	tests := []struct {
+		name    string
+		t       *TenableClient
+		args    args
+		want    *Response
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.t.Get(tt.args.ctx, tt.args.u, tt.args.opts, tt.args.dest)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TenableClient.Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TenableClient.Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTenableClient_Post(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		u    string
+		opts *TenableQueryOpts
+		body interface{}
+		dest interface{}
+	}
+	tests := []struct {
+		name    string
+		t       *TenableClient
+		args    args
+		want    *Response
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.t.Post(tt.args.ctx, tt.args.u, tt.args.opts, tt.args.body, tt.args.dest)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TenableClient.Post() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TenableClient.Post() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTenableClient_SetHttpClient(t *testing.T) {
+	type args struct {
+		client *http.Client
+	}
+	tests := []struct {
+		name string
+		t    *TenableClient
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.t.SetHttpClient(tt.args.client)
+		})
+	}
+}
+
+func TestTenableClient_SetBaseUrl(t *testing.T) {
+	type args struct {
+		baseUrl string
+	}
+	tests := []struct {
+		name string
+		t    *TenableClient
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.t.SetBaseUrl(tt.args.baseUrl)
+		})
+	}
+}
+
+func TestTenableClient_ImpersonateAs(t *testing.T) {
+	type args struct {
+		username string
+	}
+	tests := []struct {
+		name string
+		t    *TenableClient
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.t.ImpersonateAs(tt.args.username)
 		})
 	}
 }
